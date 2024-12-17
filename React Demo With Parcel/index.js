6b636f8d353cbb7adef98e3c078e2352b6f540df
Yogendra <a.yogendra65@gmail.com>
2024-12-17 23:59:34 +0530

    Updated JSX code

--- React Demo With Parcel/index.js
+++ React Demo With Parcel/index.js
@@ -3,42 +3,85 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+let name = "Yogi"
+
 const Header = function () {
-    return React.createElement('h1', null, "Welcome Yogi");
+    return <h1>Welcome {name}</h1>
 }
 
-const App = function () {
-    return React.createElement(
-        'table',
-        { border: 1 },
-        React.createElement('thead', null,
-            React.createElement('tr', null,
-                React.createElement('th', null, "Name"),
-                React.createElement('th', null, "Age")
-            )
-        ),
-        React.createElement('tbody', null,
-            React.createElement('tr', null,
-                React.createElement('td', null, "Yogi"),
-                React.createElement('td', null, 22)
-            ),
-            React.createElement('tr', null,
-                React.createElement('td', null, "Prasad"),
-                React.createElement('td', null, 21)
-            ),
-            React.createElement('tr', null,
-                React.createElement('td', null, "Naveen"),
-                React.createElement('td', null, 21)
-            )
-        )
+const Form = function () {
+    return (
+        <form>
+            <div>
+                <label>Name: </label>
+                <input type='text' placeholder={name}></input>
+            </div>
+
+            <div>
+                <label>Age: </label>
+                <input type='text'></input>
+            </div>
+
+            <div>
+                <label>Gender: </label>
+                <select>
+                    <option>Male</option>
+                    <option>Female</option>
+                    <option>Other</option>
+                </select>
+            </div>
+        </form>
     );
 }
 
-const Main = function () {
-    return React.createElement('div', null,
-        React.createElement(Header),
-        React.createElement(App),
+const Table = function () {
+    const tableStyle = {
+        borderCollapse: "collapse",
+        width: "50%",
+        margin: "0 auto",
+        textAlign: "center"
+    };
+
+    const cellStyle = {
+        border: "1px solid black",
+        padding: "8px"
+    };
+
+    const headerCellStyle = {
+        ...cellStyle,
+        backgroundColor: "lightgray",
+        fontWeight: "bold"
+    };
+
+    return (
+        <>
+            <Header />
+
+            <Form />
+
+            <table style={tableStyle}>
+                <thead>
+                    <tr>
+                        <th style={headerCellStyle}>Name</th>
+                        <th style={headerCellStyle}>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style={cellStyle}>Yogi</td>
+                        <td style={cellStyle}>22</td>
+                    </tr>
+                    <tr>
+                        <td style={cellStyle}>Prasad</td>
+                        <td style={cellStyle}>21</td>
+                    </tr>
+                    <tr>
+                        <td style={cellStyle}>Eswar</td>
+                        <td style={cellStyle}>23</td>
+                    </tr>
+                </tbody>
+            </table></>
     );
 }
 
-root.render(React.createElement(Main));
+root.render(<Table />);
